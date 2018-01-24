42c069f4e1923fe13560abdfd95b84ac75fcd3c9
deeCodeMonkey <deidra108@gmail.com>
2018-01-24 16:48:56 -0700

    security complete

--- server/common.js
+++ server/common.js
@@ -1,4 +1,11 @@
-import { Meteor } from 'meteor/meteor';
+//when autopublish removed
+Meteor.publish('products', function () {
+    return Products.find();
+});
+
+Meteor.publish('categories', function () {
+    return Categories.find();
+});
 
 Meteor.startup(() => {
   // code to run on server at startup
